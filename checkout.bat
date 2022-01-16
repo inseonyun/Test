800eaeee2a3a68451c82894d5c1c430e8f04a8a2
inseonyun <84364741+inseonyun@users.noreply.github.com>
2022-01-16 16:56:05 +0900

    Add a batch files

---
+++ checkout.bat
@@ -0,0 +1,4 @@
+cd d:
+cd d:/2021_2_CapstoneDesign/DingDong
+git pull
+git checkout -b batch
---
+++ push.bat
@@ -0,0 +1,4 @@
+cd d:/2021_2_CapstoneDesign/DingDong
+git add .
+git commit -m "Added from batch file"
+git push origin batch
